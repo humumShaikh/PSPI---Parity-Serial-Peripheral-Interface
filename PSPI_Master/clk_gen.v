a7282815c3098f7fce9e17ebbd09e642f95a7a60
HUMUM <humumshaikh0@gmail.com>
2025-12-01 11:42:21 +0530

    Update clk_gen.v

--- PSPI_Master/clk_gen.v
+++ PSPI_Master/clk_gen.v
@@ -1,3 +1,16 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: CWIR3
+// Engineer: copperwire
+// Design Name: main
+// Module Name: main
+// Project Name: PSPI - Master
+// Target Devices: xc7s50csga324-1 (Boolean)
+// Description:    Top module for the PSPI Master 
+// Dependencies:
+// Revision 0.01 - File Created
+// Additional Comments:
+//////////////////////////////////////////////////////////////////////////////////
+
 module clk_gen(
     input wire run,
     input wire clk_in,
