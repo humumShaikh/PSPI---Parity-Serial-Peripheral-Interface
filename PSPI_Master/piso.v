314ebc34c688fdbee26dc76f8b3f9efe9aae4b4e
HUMUM <humumshaikh0@gmail.com>
2025-12-08 23:37:25 +0530

    Update piso.v

--- PSPI_Master/piso.v
+++ PSPI_Master/piso.v
@@ -4,7 +4,7 @@
 // Design Name: piso
 // Module Name: piso
 // Project Name: PSPI - Master
-// Target Devices: xc7s50csga324-1 (Boolean)
+// Target Devices: xc7s50csga324-1 (Boolean Board by Real Digital)
 // Description:  Parallel In Serial Out shift register for the PSPI Master  
 // Dependencies:
 // Revision 0.01 - File Created
