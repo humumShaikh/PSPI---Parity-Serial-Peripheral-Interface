189bc019a25669a2a26910683055c26e59eefec4
HUMUM <humumshaikh0@gmail.com>
2025-12-08 23:36:52 +0530

    Update sipo.v

--- PSPI_Master/sipo.v
+++ PSPI_Master/sipo.v
@@ -6,7 +6,7 @@
 // Project Name: PSPI - Master
 // Target Devices: xc7s50csga324-1 (Boolean)
 // Description:    Serial In Parallel Out Shift Register for the PSPI Master , takes the data through the MISO pin serially and outputs it parallely on the onboard LEDs 
-// Dependencies: 
+// Dependencies: (clk_gen) for clocking
 // Revision 0.01 - File Created
 // Additional Comments:
 //////////////////////////////////////////////////////////////////////////////////
